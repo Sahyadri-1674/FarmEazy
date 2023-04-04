5651ae777f584bc27b7563fa0065842247396004
ankitdubetsec <116506238+ankitdubetsec@users.noreply.github.com>
2023-04-04 18:14:42 +0530

    changed style of seed and ferti

--- seeddcalculator.py
+++ seeddcalculator.py
@@ -50,37 +50,37 @@ bg_image = ImageTk.PhotoImage(bg_image)
 # Create a Label widget to hold the background image
 bg_label = tk.Label(root, image=bg_image)
 bg_label.place(x=0, y=0, relwidth=1, relheight=1)
-nav_frame = tk.Frame(root, bg="#576CBC")
+nav_frame = tk.Frame(root, bg="#badc57")
 nav_frame.place(relx=0,rely=0,relwidth=1,relheight=0.08)
 
 # Create a label for the title on the left corner
-title_label = tk.Label(nav_frame, text="Farm Eazy", bg="#576CBC", fg="white", padx=10,font='Helvetica 18 bold')
+title_label = tk.Label(nav_frame, text="Farm Eazy", bg="#badc57", fg="black", padx=10,font='Helvetica 18 bold')
 title_label.pack(side="left")
 
 # Create a frame for the buttons on the right
-button_frame = tk.Frame(nav_frame, bg="#576CBC")
+button_frame = tk.Frame(nav_frame, bg="#badc57")
 button_frame.pack(side="right")
 
 # Create three buttons
-button1 = tk.Button(button_frame, text="Home", bg="#576CBC", fg="white", padx=10, borderwidth=0, highlightthickness=0,font='Helvetica 14 bold')
+button1 = tk.Button(button_frame, text="Home", bg="#badc57", fg="black", padx=10, borderwidth=0, highlightthickness=0,font='Helvetica 14 bold')
 button1.pack(side="right", padx=10)
 
-button2 = tk.Button(button_frame, text="Log Out", bg="#576CBC", fg="white", padx=10, borderwidth=0, highlightthickness=0,font='Helvetica 14 bold')
+button2 = tk.Button(button_frame, text="Log Out", bg="#badc57", fg="black", padx=10, borderwidth=0, highlightthickness=0,font='Helvetica 14 bold')
 button2.pack(side="right", padx=10)
 
-button3 = tk.Button(button_frame, text="About us", bg="#576CBC", fg="white", padx=10, borderwidth=0, highlightthickness=0,font='Helvetica 14 bold')
+button3 = tk.Button(button_frame, text="About us", bg="#badc57", fg="black", padx=10, borderwidth=0, highlightthickness=0,font='Helvetica 14 bold')
 button3.pack(side="right", padx=10)
 
 # Create the login form
-frame = tk.LabelFrame(root, bg="#576CBC")
+frame = tk.LabelFrame(root, bg="#badc57")
 frame.place(relx=0.3, rely=0.55, relwidth=0.4, relheight=0.75, anchor="center")
-frame.configure(borderwidth=3)
+frame.configure(borderwidth=3,bd=10)
 # root.attributes('-alpha', 0.5)
 
 # F1 = LabelFrame(frame, text="Customer Details", font=('times new roman', 15, 'bold'), bd=10, fg="Black", bg="#badc57")
 # F1.place(x=0, y=80, relwidth=100)
 # Create labels and entry widgets for username and password
-area_label = tk.Label(frame, text="Area:", bg="#576CBC" ,font='Helvetica 18 bold',fg="white")
+area_label = tk.Label(frame, text="Area:", bg="#badc57" ,font='Helvetica 18 bold',fg="black")
 area_label.place(relx=0.1, rely=0.2, relheight=0.1)
 
 area_entry = tk.Entry(frame,textvariable="enter area in acres",font='Helvetica 18 bold',borderwidth=3)
@@ -88,7 +88,7 @@ area_entry.place(relx=0.3, rely=0.2, relwidth=0.5, relheight=0.1)
 
 
 
-crop_label = tk.Label(frame, text="crop:", bg="#576CBC",font='Helvetica 18 bold',fg="white")
+crop_label = tk.Label(frame, text="crop:", bg="#badc57",font='Helvetica 18 bold',fg="black")
 crop_label.place(relx=0.1, rely=0.4, relheight=0.1)
 
 # crop_entry = ttk.Entry(frame)
