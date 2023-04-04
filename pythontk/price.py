90c7341141c6a1f9dfde9182294ab5e0f831f940
Educatoraman <128976778+Educatoraman@users.noreply.github.com>
2023-04-04 01:45:23 -0700

    Delete pythontk directory

--- pythontk/price.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-dict = {
-    "Crop_Name":['Pear','Pumpkin','Cabbage','Carrot'],
-    "Price(Per_Quintal)":[8300,1300,550,1200]
-    }
-df = pd.DataFrame(dict)
-df
